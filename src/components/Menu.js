4e1a2a2c7aab8127482f7587e7ce4356301de7e5
CodingCupcake <brjezunigalo@ittepic.edu.mx>
2025-01-14 18:33:53 -0700

    proyecto funcional - seguir trabajando

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -403,14 +403,20 @@ const MenuData = [
     },  
 ]
 
-const Menu = () => {
-    return(
+const Menu = ({ selectedCategory }) => {
+    const filteredData = MenuData.filter(item => item.cat === selectedCategory);
+
+    return (
         <div className="bg-gray-100 py-10">
             <div className="container">
                 <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
-                    {MenuData.map((item, index) => (
-                        <Card key={index} {...item} />
-                    ))}
+                    {filteredData.length > 0 ? (
+                        filteredData.map((item, index) => (
+                            <Card key={index} {...item} />
+                        ))
+                    ) : (
+                        <p></p>
+                    )}
                 </div>
             </div>
         </div>
